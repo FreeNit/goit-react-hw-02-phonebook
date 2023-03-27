159877bfe368faa4d6670839a4994ffa58d5e588
Dmyto Kovach <bolilui.dmytro@gmail.com>
2023-03-27 23:54:04 +0300

    Make input as controlled element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    const form = evt.currentTarget;
+    const { name } = form.elements;
+
+    const contact = {
+      id: nanoid(),
+      name: name.value,
+    };
+
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, { contact }],
+      };
+    });
+  };
+
+  handleChange = evt => {
+    this.setState({ name: evt.target.value });
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        {/* // ! Component */}
+        <h3>Phonebook</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        {/* // ! Component */}
+        <h3>Contacts</h3>
+        <ul></ul>
+      </div>
+    );
+  }
+}
