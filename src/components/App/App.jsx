18fe7b1e98b460998c216c6c23d86afbf0f97a36
Dimon <bolilui.dmytro@gmail.com>
2023-03-28 22:11:49 +0300

    Remove console.log

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -29,7 +29,6 @@ export class App extends Component {
   };
 
   deleteContact = id => {
-    console.log(id);
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
